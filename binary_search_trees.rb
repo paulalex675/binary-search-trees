f735c8a0662412f7edc414cb747c59c98c340401
Paul Robinson <paulalex675@hotmail.com>
2021-01-20 02:30:47 +0000

    played with counting leaf nodes in the hope it will help calculate height and generally understand more

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,5 +1,6 @@
 class Node
-    attr_accessor :left, :right, :data
+  include Comparable
+    attr_accessor :left, :right, :data, :leaf_node
     
     def initialize(data, left, right)
         @data = data
@@ -9,11 +10,15 @@ class Node
 end
 
 class BinaryTree
-    attr_accessor :root, :value
+    attr_accessor :root, :value, :leaf_nodes
 
     def initialize(ary)
         @value = merge_sort(ary)
-        @root = build_tree(ary)
+        @root = build_tree(@value)
+        @leaf_nodes = Array.new
+        leaf_node_count
+        depth_counter = 0
+        height_counter  = 0
     end
     
     def merge_sort(array)
@@ -41,26 +46,57 @@ class BinaryTree
     end
 
     def build_tree(array)
+      return nil if array.empty?
+
       if array.length <= 1
         @root = Node.new(array[0], nil, nil)
-      elsif array.length > 1 && array.length < 3
-        @root = Node.new(array[1], Node.new(array[0], nil, nil), nil)
       else
         mid = array.length/2
-        @root = Node.new(array[mid], build_tree(array.slice(0...mid)), build_tree(array.slice(mid+1..-1)))
+        @root = Node.new(array[mid], build_tree(array.slice(0...mid)), build_tree(array.slice(mid+1..array.length)))
       end
     end
 
-    def insert(value)
-
+    def insert(value, node = self.root)
+      if value == node.data
+        return 'That node already exists'
+      elsif value < node.data
+        if node.left == nil
+          node.left = Node.new(value, nil, nil)
+        else insert(value, node.left)
+        end
+      else
+        if node.right == nil
+          node.right = Node.new(value, nil, nil)
+        else insert(value, node.right)
+        end
+      end
     end
 
-    def delete(value)
-
+    def delete(value, node = self.root)
+      if node == nil
+        return "Looked for #{value} but no match found"
+      elsif node.left == nil && node.right == nil
+        node = nil
+      elsif value < node
+        delete(value, node.left)
+      elsif value > node
+        delete(value, node.right)
+      else
+      end
     end
 
-    def find(value)
-
+    def find(value, node = self.root)
+      if node == nil
+        return "Looked for #{value} but no match found"
+      elsif value == node.data
+        return node
+      elsif value < node.data
+        find(value, node.left)
+      elsif value > node.data
+        find(value, node.right)
+      else
+        return "Looked for #{value} but no match found"
+      end
     end
 
     def level_order(value)
@@ -79,8 +115,36 @@ class BinaryTree
 
     end
 
-    def height(value)
+    def calculate_LH(value)
+      node = find(value)
+      if node.left == nil
+        counter_l += 1
+      else
+        calculate_LH(node.left) 
+        counter_l += 1
+      end
+    end
 
+    def calculate_RH(value)
+      node = find(value)
+      if node.right == nil
+        counter_r += 1
+      else 
+        calculate_RH(node.right)
+        counter_r += 1
+      end
+    end
+
+    def height(value)
+      counter_r = 0
+      counter_l = 0
+      calculate_LH(value)
+      calculate_RH(value)
+      if counter_l < counter_r
+         puts counter_r 
+      else 
+        puts counter_l
+      end
     end
 
     def depth(value)
@@ -95,8 +159,41 @@ class BinaryTree
 
     end
 
+    def leaf_node(value)
+      node = find(value)
+      if node.left == nil && node.right == nil
+         puts "#{value} is a leaf node" 
+      else
+        puts "#{value} is not a leaf node"
+      end
+    end
+
+    def leaf_node_count(node = self.root)
+      if node.left == nil && node.right == nil
+        leaf_nodes << node
+      elsif node.left != nil && node.right != nil
+        leaf_node_count(node.left)
+        leaf_node_count(node.right)
+      elsif node.left != nil && node.right == nil
+        leaf_node_count(node.left)
+      else node.left == nil && node.right != nil
+        leaf_node_count(node.right)        
+      end
+    end
+
+    def how_many_leaves?
+      puts "There are #{leaf_nodes.length} leaf nodes in this tree"
+    end
+
 end
 
-test = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+test = [1, 2, 3, 4, 45, 6, 7, 8, 9, 10, 6, 2, 34, 17]
 tree1 = BinaryTree.new(test)
+#tree1 = BinaryTree.new(Array.new(15) { rand(1..100) })
+#pp tree1
+#puts tree1.find(4)
+#puts tree1.insert(5)
 pp tree1
+tree1.how_many_leaves?
+tree1.leaf_node(10)
+print tree1.height(7)
